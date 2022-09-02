d5d9a0c86d11dbed4ac8ab4d32189677eb44f5ab
Vasil Pashov <vasil.pashov1@gmail.com>
2022-09-03 00:34:06 +0300

    Initial commit

---
+++ cpp/cmd_line_args.cpp
@@ -0,0 +1,206 @@
+#include "cmd_line_args.h"
+#include <cstring>
+#include <cassert>
+#include <cstdio>
+#include <format>
+
+namespace CMD {
+
+CommandLineArgs::ParamInfo::ParamInfo(
+	const char* description,
+	const Type type,
+	const bool required
+) :
+	description(std::string(description)),
+	type(type),
+	required(required)
+{ }
+
+CommandLineArgs::ErrorCode CommandLineArgs::parse(const int argc, char** argv, void(*errorCallback)(ErrorCode, const char* param, const char* error)) {
+    // argv[0] is the name of the program (exe), that is why the for starts from 1
+    for (int i = 1; i < argc; ++i) {
+        const char* arg = argv[i];
+        if (arg[0] == '-') {
+            int j = 1;
+            bool hasParams = false;
+            while (arg[j]) {
+                if (arg[j] == '=') {
+                    hasParams = true;
+                    break;
+                }
+                j++;
+            }
+
+            // -someArg=someVal -> arg + 1 to start past the '-' from 's'
+            // j-1 is the length, subtract 1 to account for the parameter
+            const int nameLen = j - 1;
+            std::string name(arg + 1, nameLen);
+            InfoConstIt it = paramInfo.find(name);
+            if (it == paramInfo.end()) {
+                const ErrorCode code = ErrorCode::UnknownParameter;
+                if (errorCallback) {
+                    const std::string& error = std::format("Unknown parameter {}", name);
+                    errorCallback(code, name.c_str(), error.c_str());
+                }
+                return code;
+            }
+            if (!hasParams && it->second.type == Type::Flag) {
+                paramValues[name] = ParamVal();
+                continue;
+            } else if (hasParams && it->second.type == Type::Flag) {
+                const ErrorCode code = ErrorCode::FlagHasValue;
+                if (errorCallback) {
+                    const std::string& error = std::format(
+                        "Parameter {} is a flag parameter. It does not have value!"
+                        "Received input is: {}",
+                        name.c_str(),
+                        arg
+                    );
+                    errorCallback(code, name.c_str(), error.c_str());
+                }
+                return code;
+            } else if (!hasParams && it->second.type != Type::Flag) {
+                const ErrorCode code = ErrorCode::MissingValue;
+                if (errorCallback) {
+                    const std::string& error = std::format(
+                        "Parameter must receive value. The syntax is -parameter=value (no spaces surrounding =)"
+                        "Received input is: {}",
+                        arg
+                    );
+                    errorCallback(code, name.c_str(), error.c_str());
+                }
+                return code;
+            }
+            
+            ParamVal val;
+            const int paramStartIndex = j + 1;
+            switch (it->second.type) {
+                case Type::Int: {
+                    char* next;
+                    const int res = strtol(arg + paramStartIndex, &next, 0);
+                    // strtol returns 0 if it cannot parse, so there are two possibilities to get 0
+                    // a) It could not parse
+                    // b) It parsed a zero
+                    // So check if the number is 0
+                    // second parameter of strtol returns which is the next char after the last parsed char
+                    // we want it to be the end of the string
+                    if ((res == 0 && arg[j + 1] != '0') || *next != '\0') {
+                        const ErrorCode code = ErrorCode::WrongValueType;
+                        if (errorCallback) {
+                            const std::string& error = std::format(
+                                "Wrong parameter value. Expected: -{}=[INTEGER_VALUE]"
+                                "Received input is: {}",
+                                arg,
+                                arg
+                            );
+                            errorCallback(ErrorCode::WrongValueType, arg, error.c_str());
+                        }
+                        return code;
+                    }
+                    val.intVal = res;
+                } break;
+                case Type::String: {
+                    // Don't forget it starts with '-'
+                    const int len = strlen(arg) - j;
+                    val.stringVal = new char[len + 1];
+                    strcpy(val.stringVal, arg + paramStartIndex);
+                    val.stringVal[len] = '\0';
+                } break;
+                default: {
+                    assert(false);
+                    const ErrorCode code = ErrorCode::Unknown;
+                    if (errorCallback) {
+                        const std::string& error = std::format(
+                            "Unknown input %s. All arguments must start with \'-\' and use \'=\' for setting value."
+                            "There must be no intervals surrounding \'=\' or after the \'-\'!",
+                            arg
+                        );
+                        errorCallback(code, name.c_str(), error.c_str());
+                    }
+                    return code;
+                }
+            }
+            paramValues[name] = val;
+        } else {
+            const ErrorCode code = ErrorCode::Unknown;
+            if (errorCallback) {
+                const std::string& error = std::format(
+                    "Unknown input %s. All arguments must start with \'-\' and use \'=\' for setting value."
+                    "There must be no intervals surrounding \'=\' or after the \'-\'!",
+                    arg
+                );
+                errorCallback(code, arg, error.c_str());
+            }
+            return code;
+        }
+    }
+    for (auto& it : paramInfo) {
+        if (it.second.required && paramValues.find(it.first) == paramValues.end()) {
+            const ErrorCode code = ErrorCode::MissingParameter;
+            if (errorCallback) {
+                const std::string& error = std::format("Required parameter {} cannot be found!", it.first.c_str());
+                errorCallback(ErrorCode::MissingParameter, it.first.c_str(), error.c_str());
+            }
+            return code;
+        }
+    }
+    return ErrorCode::Success;
+}
+
+CommandLineArgs::~CommandLineArgs() {
+	freeMem();
+}
+
+void CommandLineArgs::addParam(
+	const char* name,
+	const char* description,
+	const Type type,
+	const bool required
+) {
+	paramInfo[std::string(name)] = ParamInfo(description, type, required);
+}
+
+const int* CommandLineArgs::getIntVal(const char* name) const {
+	const std::string paramName(name);
+	ValuesConstIt it = paramValues.find(paramName);
+	if (it != paramValues.end()) {
+		return &it->second.intVal;
+	}
+	return nullptr;
+}
+
+const char* CommandLineArgs::getStringVal(const char* name) const {
+	const std::string paramName(name);
+	ValuesConstIt it = paramValues.find(paramName);
+	if (it != paramValues.end()) {
+		return it->second.stringVal;
+	}
+	return nullptr;
+}
+
+bool CommandLineArgs::isSet(const char* name) const {
+	return paramValues.find(std::string(name)) != paramValues.end();
+}
+
+void CommandLineArgs::freeValues() {
+	for (auto& it : paramValues) {
+		const bool isString = paramInfo[it.first].type == Type::String;
+		if (isString) {
+			delete[] it.second.stringVal;
+		}
+	}
+	paramValues.clear();
+}
+
+void CommandLineArgs::freeMem() {
+	freeValues();
+	paramInfo.clear();
+}
+
+void CommandLineArgs::print(FILE* f) {
+	for (auto& it : paramInfo) {
+		fprintf(f, "%s - %s\n", it.first.c_str(), it.second.description.c_str());
+	}
+}
+
+} // namespace CMD
---
+++ include/cmd_line_args.h
@@ -0,0 +1,75 @@
+#pragma once
+#include <unordered_map>
+#include <string>
+
+namespace CMD {
+
+/// Structure to parse and hold command line arguments
+struct CommandLineArgs {
+	~CommandLineArgs();
+	enum class Type {
+		Int,
+		String,
+		Flag
+	};
+
+	enum class ErrorCode {
+		Success = 0,
+		MissingParameter,
+		WrongValueType,
+		UnknownParameter,
+		FlagHasValue,
+		MissingValue,
+		Unknown
+	};
+
+	/// parse command line arguments that come from main
+	/// @param[in] numArgs the number of the arguments
+	/// @param[in] args  array of arguments that comes from main function where
+	/// args[0] is the name of the program, args[1] is the first argument and so on
+	ErrorCode parse(const int numArgs, char** args, void(*errorCallback)(ErrorCode err, const char* param, const char* error) = nullptr);
+	/// Add a info for possible parameter that can come from the command line. All possible parameters must
+	/// be added before calling parse. Calling this does not add a parameter only information.
+	/// @param[in] name name of the param, must not include - or =.
+	/// @param[in] description description for the parameter can include everything. This is shown when calling print
+	/// @param[in] type type of the parameter. Int types are extracted with getIntVal, string tipes with getStringVal ...
+	/// @param[in] required if true the parameter is required. If a parameter is required but not found parse will return error
+	void addParam(const char* name, const char* description, const Type type, const bool required);
+	/// Try to extract parameter.
+	/// @param[in] name the name of the parameter
+	/// @retval pointer to the value or nullptr if the param is not present
+	const int* getIntVal(const char* name) const;
+	/// Try to extract parameter.
+	/// @param[in] name the name of the parameter
+	/// @retval pointer to the value or nullptr if the param is not present
+	const char* getStringVal(const char* name) const;
+	/// Check if the parameter has been parsed. This is the way to check if flag parameter (param with no type) has been set.
+	/// @param[in] name the name of the parameter
+	/// @retval true if the parameter was parsed, false otherwise
+	bool isSet(const char* name) const;
+	/// Show the parameters with their description
+	/// @param[in,out] f File stream where parameters will be printed
+	void print(FILE* f);
+	/// Delete all parameters as well as all parameter info
+	void freeMem();
+	/// Delete only the parameters, without deleting the info
+	void freeValues();
+private:
+	struct ParamInfo {
+		ParamInfo() = default;
+		ParamInfo(const char* description, const Type type, const bool required);
+		std::string description;
+		Type type;
+		bool required;
+	};
+	union ParamVal {
+		int intVal;
+		char* stringVal;
+	};
+	std::unordered_map<std::string, ParamInfo> paramInfo;
+	std::unordered_map<std::string, ParamVal> paramValues;
+	using ValuesConstIt = std::unordered_map<std::string, ParamVal>::const_iterator;
+	using InfoConstIt = std::unordered_map<std::string, ParamInfo>::const_iterator;
+};
+
+}
